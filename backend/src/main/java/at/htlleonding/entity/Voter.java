99df70364f210d449ccb76e3db916b8b72c1dc57
FroellerFelix <felix.froeller@gmail.com>
2023-11-02 17:59:05 +0100

    chaned naming in Voter and cld

--- backend/src/main/java/at/htlleonding/entity/Voter.java
+++ backend/src/main/java/at/htlleonding/entity/Voter.java
@@ -2,26 +2,26 @@ package at.htlleonding.entity;
 
 public class Voter {
     //<editor-fold desc="Fields">
-    private String studentId; // The ifxxxxxx ids every student has
+    private String schoolId; // The ifxxxxxx ids every student has
     private String password; // Hash of the password (should be similar to the one in the school db)
     private boolean hasVoted;
     //</editor-fold>
 
-    //<editor-fold desc="Constructor">
+    //<editor-fold desc="Constructors">
     public Voter(String studentId, String password, boolean hasVoted) {
-        setStudentId(studentId);
+        setSchoolId(studentId);
         setPassword(password);
         setHasVoted(hasVoted);
     }
     //</editor-fold>
 
     //<editor-fold desc="Getter and Setter">
-    public String getStudentId() {
-        return studentId;
+    public String getSchoolId() {
+        return schoolId;
     }
 
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
     }
 
     public String getPassword() {
