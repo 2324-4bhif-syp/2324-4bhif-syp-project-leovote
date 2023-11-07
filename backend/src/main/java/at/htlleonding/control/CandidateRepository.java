6b070ca39472ca9df442d5e2df43cb4b2ed55831
CaoAnton <antoncao1510@gmail.com>
2023-11-07 13:07:11 +0100

    feat: create csv for candidates

--- backend/src/main/java/at/htlleonding/control/CandidateRepository.java
+++ backend/src/main/java/at/htlleonding/control/CandidateRepository.java
@@ -8,24 +8,10 @@ import java.util.List;
 
 @ApplicationScoped
 public class CandidateRepository {
-    private List<Candidate> candidates;
-
-    public CandidateRepository() {
-        candidates = new ArrayList<>();
-        createExampleData();
-    }
-
-    private void createExampleData() {
-        candidates.add(new Candidate("if20020", "Max", "Mustermann", "4BHIF"));
-        candidates.add(new Candidate("if22320", "David", "Graf", "3AHITM"));
-        candidates.add(new Candidate("if26765", "Gustav", "Eder", "5AHIF"));
-        candidates.add(new Candidate("if23757", "Ella", "Lang", "3CHIF"));
-        candidates.add(new Candidate("if22781", "Lena", "Zauner", "2BHIF"));
-    }
+    private List<Candidate> candidates = new ArrayList<>();
     public List<Candidate> getAllCandidates() {
         return candidates;
     }
-
     public void addCandidate(Candidate candidate) {
         candidates.add(candidate);
     }
--- backend/src/main/java/at/htlleonding/control/InitBean.java
+++ backend/src/main/java/at/htlleonding/control/InitBean.java
@@ -1,13 +1,44 @@
 package at.htlleonding.control;
 
+import at.htlleonding.entity.Candidate;
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class InitBean {
+    private final String inputPath = "src/main/resources/candidates.csv";
+    @Inject
+    CandidateRepository candidateRepository;
     @Transactional
     void startUp(@Observes StartupEvent event) {
         Log.info("It is working!");
-    }
+        Path inputCSVPath = Paths.get(inputPath);
+        try {
+            Log.info(String.format("Started reading from path %s", inputCSVPath));
+            List<String> lines = Files.readAllLines(inputCSVPath, StandardCharsets.UTF_8);
+            Log.info(String.format("Finish reading from path %s", inputCSVPath));
+            Log.info("Parsing: started parsing csv file into candidate objects");
+            for (String line : lines) {
+                String[] elements = line.split(";");
+                candidateRepository.addCandidate(
+                        new Candidate(
+                                elements[0],
+                                elements[1],
+                                elements[2],
+                                elements[3]
+                        )
+                );
+            }
+        } catch (Exception e) {
+            Log.error(String.format("FAILED:", inputCSVPath, e.getMessage()));
+        }
+        Log.info("Setup: completed setup");}
 }
