590e30eafac2e6b4260b814e707e21497f060624
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-11-07 14:45:39 +0100

    -fix: made timestamp in Block, to make the Blockchain more safe

--- backend/src/main/java/at/htlleonding/entity/Block.java
+++ backend/src/main/java/at/htlleonding/entity/Block.java
@@ -1,29 +1,33 @@
 package at.htlleonding.entity;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class Block {
-    private int previousHash;
     private int blockHash;
-    private String data;
+    private final int previousHash;
+    private final Date timeStamp = new Date();
+    private final String data;
 
     public Block (int previousHash, String data){
         this.previousHash = previousHash;
         this.data = data;
 
-        Object[] contens = {Arrays.hashCode(data.toCharArray()), previousHash};
+        Object[] contens = {Arrays.hashCode(data.toCharArray()), previousHash, Arrays.hashCode(timeStamp.toString().toCharArray())};
         this.blockHash = Arrays.hashCode(contens);
     }
 
     public int getPreviousHash() {
         return previousHash;
     }
-
     public String getData() {
         return data;
     }
-
     public int getBlockHash() {
         return blockHash;
+    }
+
+    public Date getTimeStamp(){
+        return timeStamp;
     }
 }
