024a2633775b3ea20ceaae3f5067e957426e935b
CaoAnton <antoncao1510@gmail.com>
2023-11-11 14:37:02 +0100

    fix: fixed voter attribute

--- backend/src/main/java/at/htlleonding/entity/Voter.java
+++ backend/src/main/java/at/htlleonding/entity/Voter.java
@@ -4,14 +4,12 @@ public class Voter {
     //<editor-fold desc="Fields">
     private String schoolId; // The ifxxxxxx ids every student has
     private String password; // Hash of the password (should be similar to the one in the school db)
-    private boolean hasVoted;
     //</editor-fold>
 
     //<editor-fold desc="Constructors">
     public Voter(String studentId, String password, boolean hasVoted) {
         setSchoolId(studentId);
         setPassword(password);
-        setHasVoted(hasVoted);
     }
     //</editor-fold>
 
@@ -30,14 +28,6 @@ public class Voter {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public boolean isHasVoted() {
-        return hasVoted;
-    }
-
-    public void setHasVoted(boolean hasVoted) {
-        this.hasVoted = hasVoted;
     }
     //</editor-fold>
 }
