88074807f72e73898207569f0e32bb69aadea667
CaoAnton <antoncao1510@gmail.com>
2023-11-11 16:37:09 +0100

    feat: add candidate component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CandidateComponent } from './candidate/candidate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateComponent
   ],
   imports: [
     BrowserModule
