5a11d3635f6834c0f1f5e2a7effb1c7e118e79db
CaoAnton <antoncao1510@gmail.com>
2023-11-13 12:00:53 +0100

    feat: create entity and service

---
+++ frontend/src/app/shared/control/candidate.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Candidate} from "../entity/candidate-model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidateService {
+  private url: string = 'http://localhost:8080/candidates';
+  private candidates: Candidate[] =[];
+  constructor() { }
+  public async getCandidates(): Promise<Candidate[]> {
+    /*let response = await fetch(this.url);
+    let data = await response.json();
+    this.candidates = data.map((candidate: any) => {
+      let customer: Candidate = {
+        schoolId: candidate.schoolId,
+        firstName: candidate.firstName,
+        lastName: candidate.lastName,
+        grade: candidate.grade
+      }
+      return customer;
+    });*/
+    return this.candidates;
+  }
+  public async createCandidate(candidate: Candidate) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    try {
+      let response = await fetch(this.url, {
+        body: JSON.stringify(candidate),
+        headers: headers,
+        method: "POST"
+      });
+
+      if(response.status !== 201) {
+        console.error("Error, could not add candidate");
+      }
+    } catch (error) {
+      console.error("Error, could not add candidate");
+    }
+    this.candidates.push(candidate);
+  }
+}
