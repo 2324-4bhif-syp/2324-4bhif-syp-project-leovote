c8f8de2b67a6204cd3e1a72a0ed12820efaac771
FroellerFelix <felix.froeller@gmail.com>
2023-11-17 19:19:56 +0100

    added initData() in InitBean.java - initData add testing data on startup

--- backend/src/main/java/at/htlleonding/control/InitBean.java
+++ backend/src/main/java/at/htlleonding/control/InitBean.java
@@ -1,21 +1,68 @@
 package at.htlleonding.control;
 
-import at.htlleonding.entity.Candidate;
+import at.htlleonding.entity.*;
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class InitBean {
     @Inject
     CandidateRepository candidateRepository;
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public void initData(@Observes StartupEvent event) {
+        // Create and persist elections
+        Election election1 = new Election(
+                "Student Council Election",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(7),
+                "General");
+        Election election2 = new Election(
+                "Class Representative Election",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(7),
+                "Class"
+        );
+        entityManager.persist(election1);
+        entityManager.persist(election2);
+
+        // Create and persist candidates
+        Candidate candidate1 = new Candidate("id123", "John", "Doe", "12A");
+        Candidate candidate2 = new Candidate("id456", "Jane", "Smith", "11B");
+        entityManager.persist(candidate1);
+        entityManager.persist(candidate2);
+
+        // Create and persist voters
+        Voter voter1 = new Voter("student1", "password1");
+        Voter voter2 = new Voter("student2", "password2");
+        entityManager.persist(voter1);
+        entityManager.persist(voter2);
+
+        // Create and persist votes
+        Vote vote1 = new Vote(candidate1, election1);
+        Vote vote2 = new Vote(candidate2, election1);
+        entityManager.persist(vote1);
+        entityManager.persist(vote2);
+
+        // Create and persist hasVoted entries
+        HasVoted hasVoted1 = new HasVoted(election1, voter1, true);
+        HasVoted hasVoted2 = new HasVoted(election1, voter2, true);
+        entityManager.persist(hasVoted1);
+        entityManager.persist(hasVoted2);
+    }
+
     @Transactional
     void startUp(@Observes StartupEvent event) {
         Log.info("It is working!");
@@ -28,17 +75,11 @@ public class InitBean {
             Log.info("Parsing: started parsing csv file into candidate objects");
             for (String line : lines) {
                 String[] elements = line.split(";");
-                candidateRepository.addCandidate(
-                        new Candidate(
-                                elements[0],
-                                elements[1],
-                                elements[2],
-                                elements[3]
-                        )
-                );
+                candidateRepository.addCandidate(new Candidate(elements[0], elements[1], elements[2], elements[3]));
             }
         } catch (Exception e) {
             Log.error(String.format("FAILED: %s, %s", inputCSVPath, e.getMessage()));
         }
-        Log.info("Setup: completed setup");}
+        Log.info("Setup: completed setup");
+    }
 }
