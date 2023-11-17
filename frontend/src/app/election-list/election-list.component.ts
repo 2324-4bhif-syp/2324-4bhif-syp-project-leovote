23425f733249feabea0c4ab98b38eae8dcbdc648
Nikolaus-Alexander <rexcptn@gmail.com>
2023-11-17 16:13:15 +0100

    components generated

---
+++ frontend/src/app/election-list/election-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-election-list',
+  templateUrl: './election-list.component.html',
+  styleUrls: ['./election-list.component.css']
+})
+export class ElectionListComponent {
+
+}
