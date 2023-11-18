8fd91f1d8137e4e7a8fdc66664dfa791866fee84
FroellerFelix <felix.froeller@gmail.com>
2023-11-18 22:01:05 +0100

    chore: switch resources to generated panache ones

--- backend/src/main/java/at/htlleonding/boundary/CandidateResource.java
+++ backend/src/main/java/at/htlleonding/boundary/CandidateResource.java
@@ -1,83 +1,10 @@
 package at.htlleonding.boundary;
 
 import at.htlleonding.entity.Candidate;
-import at.htlleonding.entity.Voter;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
 
-import java.net.URI;
-import java.util.List;
+@ResourceProperties(path = "candidates")
+public interface CandidateResource extends PanacheEntityResource<Candidate, Long> {
 
-@Path("/candidates")
-public class CandidateResource {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll() {
-        List<Candidate> candidates = Candidate.listAll();
-        return Response.ok(candidates).build();
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("id") Long id) {
-        return Candidate.findByIdOptional(id)
-                .map(candidate -> Response.ok(candidate).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build());
-    }
-
-    @GET
-    @Path("school-id/{school-id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("school-id") String id) {
-        return Candidate.find("schoolId", id)
-                .singleResultOptional()
-                .map(candidate -> Response.ok(candidate).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build());
-    }
-
-    @POST
-    @Transactional
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(Candidate candidate) {
-        Candidate.persist(candidate);
-
-        if (candidate.isPersistent()) {
-            return Response.created(URI.create("/candidates" + candidate.id)).build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
-
-
-    @DELETE
-    @Transactional
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteById(@PathParam("id") Long id) {
-        boolean deleted = Candidate.deleteById(id);
-
-        if (deleted) {
-            return Response.noContent().build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
-
-    @DELETE
-    @Transactional
-    @Path("school-id/{school-id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteBySchoolId(@PathParam("school-id") String schoolId) {
-        long deletedId = Candidate.delete("schoolId", schoolId);
-
-        if (deletedId != -1) {
-            return Response.ok().entity("{\"deletedId\": " + deletedId + "}").build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
 }
--- backend/src/main/java/at/htlleonding/boundary/VoterRessource.java
+++ backend/src/main/java/at/htlleonding/boundary/VoterRessource.java
@@ -1,82 +1,10 @@
 package at.htlleonding.boundary;
 
 import at.htlleonding.entity.Voter;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
 
-import java.net.URI;
-import java.util.List;
+@ResourceProperties(path = "voters")
+public interface VoterRessource extends PanacheEntityResource<Voter, Long> {
 
-@Path("/voters")
-public class VoterRessource {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll() {
-        List<Voter> voters = Voter.listAll();
-        return Response.ok(voters).build();
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("id") Long id) {
-        return Voter.findByIdOptional(id)
-                .map(voter -> Response.ok(voter).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build());
-    }
-
-    @GET
-    @Path("school-id/{school-id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("school-id") String id) {
-        return Voter.find("schoolId", id)
-                .singleResultOptional()
-                .map(candidate -> Response.ok(candidate).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build());
-    }
-
-    @POST
-    @Transactional
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(Voter voter) {
-        Voter.persist(voter);
-
-        if (voter.isPersistent()) {
-            return Response.created(URI.create("/voters" + voter.id)).build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
-
-
-    @DELETE
-    @Transactional
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteById(@PathParam("id") Long id) {
-        boolean deleted = Voter.deleteById(id);
-
-        if (deleted) {
-            return Response.noContent().build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
-
-    @DELETE
-    @Transactional
-    @Path("school-id/{school-id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteBySchoolId(@PathParam("school-id") String schoolId) {
-        long deletedId = Voter.delete("schoolId", schoolId);
-
-        if (deletedId != -1) {
-            return Response.ok().entity("{\"deletedId\": " + deletedId + "}").build();
-        }
-
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
 }
