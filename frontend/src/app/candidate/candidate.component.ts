8bb23b3f270229523890ac86804d77aeb5260e0b
Nikolaus-Alexander <alexandernikolaus04@gmail.com>
2023-11-18 15:31:14 +0100

    CheckBox for Sample Data + election + candidate List

--- frontend/src/app/candidate/candidate.component.ts
+++ frontend/src/app/candidate/candidate.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {CandidateService} from "../shared/control/candidate.service";
 import {Candidate} from "../shared/entity/candidate-model";
 
 @Component({
@@ -8,34 +7,5 @@ import {Candidate} from "../shared/entity/candidate-model";
   styleUrls: ['./candidate.component.css']
 })
 export class CandidateComponent {
-  protected candidates: Candidate[];
-  protected newCandidate: Candidate;
-  constructor(public CS: CandidateService) {
-    this.CS = CS;
-    this.candidates = [];
-    this.initCandidates();
-    this.newCandidate = {
-      schoolId: "",
-      firstName: "",
-      lastName: "",
-      grade: ""
-    }
-  }
-  async initCandidates() {
-    this.candidates = await this.CS.getCandidates();
-    console.debug(this.candidates);
-  }
-  async clearCandidates() {
-    this.candidates = [];
-    console.debug(this.candidates);
-  }
-  async add() {
-    await this.CS.createCandidate(this.newCandidate);
-    this.newCandidate = {
-      schoolId:"",
-      firstName:"",
-      lastName: "",
-      grade:""
-    }
-  }
+  candidate:Candidate =  new Candidate("if00", "John", "Doe", "0AHIF");
 }
--- frontend/src/app/election/election.component.ts
+++ frontend/src/app/election/election.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Election} from "../shared/entity/election-model";
 
 @Component({
   selector: 'app-election',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./election.component.css']
 })
 export class ElectionComponent {
-
+ election:Election = new Election("0",new Date("01/01/2001"),new Date("01/01/2001"),"Sample");
 }
