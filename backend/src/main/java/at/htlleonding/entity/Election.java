423261f70fd7b6e40a029f69a7cf722f9a1c47f0
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-11-18 21:21:51 +0100

    -feat: overworked entities Election and Vote with Resources, to work with Panache

--- backend/src/main/java/at/htlleonding/entity/Election.java
+++ backend/src/main/java/at/htlleonding/entity/Election.java
@@ -1,22 +1,17 @@
 package at.htlleonding.entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
 @Entity
-public class Election {
+public class Election extends PanacheEntity {
     //<editor-fold desc="Fields">
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private LocalDateTime electionStart;
-    private LocalDateTime electionEnd;
-    private String electionType;
+    public String name;
+    public LocalDateTime electionStart;
+    public LocalDateTime electionEnd;
+    public String electionType;
     //</editor-fold>
 
     //<editor-fold desc="Constructors">
@@ -25,51 +20,9 @@ public class Election {
     }
 
     public Election(String name, LocalDateTime electionStart, LocalDateTime electionEnd, String electionType) {
-        setName(name);
-        setElectionStart(electionStart);
-        setElectionEnd(electionEnd);
-        setElectionType(electionType);
-    }
-    //</editor-fold>
-
-    //<editor-fold desc="Getter and Setter">
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public LocalDateTime getElectionStart() {
-        return electionStart;
-    }
-
-    public void setElectionStart(LocalDateTime electionStart) {
         this.electionStart = electionStart;
-    }
-
-    public LocalDateTime getElectionEnd() {
-        return electionEnd;
-    }
-
-    public void setElectionEnd(LocalDateTime electionEnd) {
         this.electionEnd = electionEnd;
-    }
-
-    public String getElectionType() {
-        return electionType;
-    }
-
-    public void setElectionType(String electionType) {
         this.electionType = electionType;
     }
     //</editor-fold>
