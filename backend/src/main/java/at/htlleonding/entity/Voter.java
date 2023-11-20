34ee078e76cd8f4954287595504be04cffdb06bf
FroellerFelix <felix.froeller@gmail.com>
2023-11-20 14:26:19 +0100

    chore: change panache implementation to repository pattern - effected classes: Candidate.java, Election.java, HasVoted.java, Vote.java, Voter.java

--- backend/src/main/java/at/htlleonding/entity/Voter.java
+++ backend/src/main/java/at/htlleonding/entity/Voter.java
@@ -7,10 +7,13 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Voter extends PanacheEntity {
+    //<editor-fold desc="Fields">
     @Column(unique = true)
-    public String schoolId; // The ifxxxxxx ids every student has
-    public String password; // Hash of the password (should be similar to the one in the school db)
+    private String schoolId; // The ifxxxxxx ids every student has
+    private String password; // Hash of the password (should be similar to the one in the school db)
+    //</editor-fold>
 
+    //<editor-fold desc="Constructors">
     public Voter() {
 
     }
@@ -19,4 +22,23 @@ public class Voter extends PanacheEntity {
         this.schoolId = schoolId;
         this.password = password;
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Getter and Setter">
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    //</editor-fold>
 }
