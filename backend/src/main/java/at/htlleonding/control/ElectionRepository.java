51c40de302ae36d8d1eb6837172e208fc845cd02
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-12-10 11:35:18 +0100

    -feat: made new method in ElectionRepository, which reads the Results, and returns the candidates, with percentage in a Hashmap
    Co-authored-by: Anton Cao <antoncao1510@gmail.com>

--- backend/src/main/java/at/htlleonding/control/ElectionRepository.java
+++ backend/src/main/java/at/htlleonding/control/ElectionRepository.java
@@ -9,22 +9,32 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 
 import java.util.HashMap;
-import java.util.List;
 
 @ApplicationScoped
 public class ElectionRepository implements PanacheRepository<Election> {
     EntityManager entityManager = Panache.getEntityManager();
 
     public HashMap<Candidate, Double> reviewResults (Election election){
-        /*HashMap<Candidate, Double> results = new HashMap<>();
+        HashMap<Candidate, Integer> voteCounts = new HashMap<>();
         Blockchain blockchain = new Blockchain(election.getBlockchainFileName());
-        List<Block> blocks = blockchain.getBlocks();
 
-        for(int i = 1; i < blocks.size(); i++) {
-            if(results.containsKey(blocks.get(i).getVote())){
-                results.get(blocks.get(i).getVote())
-            }
-        }*/
-        return null;
+        // Assuming the Candidate class has proper equals() and hashCode() implementations
+        for (Block block : blockchain.getBlocks()) {
+            Candidate votedCandidate = block.getVote();
+
+            // Update vote count for the candidate
+            voteCounts.put(votedCandidate, voteCounts.getOrDefault(votedCandidate, 0) + 1);
+        }
+
+        // Calculate percentages
+        int totalVotes = blockchain.getBlocks().size() - 1; // excluding genesis block
+        HashMap<Candidate, Double> results = new HashMap<>();
+        for (Candidate candidate : voteCounts.keySet()) {
+            int votes = voteCounts.get(candidate);
+            double percentage = (double) votes / totalVotes * 100.0;
+            results.put(candidate, percentage);
+        }
+
+        return results;
     }
 }
