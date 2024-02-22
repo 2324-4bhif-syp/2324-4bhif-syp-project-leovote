f1a31c52445a63394e4d31c55af420a797f55891
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2024-02-22 14:28:02 +0100

    -feat: made dropdown in taskbar has to be designed now

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import {LeovoteWebApiService} from "./shared/api/leovote-web-api.service";
+import {Component, Input} from '@angular/core';
 import {Router} from "@angular/router";
 import {VoteService} from "./shared/control/vote.service";
+import {AdminService} from "./shared/control/admin.service";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import {VoteService} from "./shared/control/vote.service";
 })
 export class AppComponent {
   title = 'Leovote';
-  constructor(public voteService: VoteService, private router: Router) {}
+  constructor(public voteService: VoteService, private router: Router, protected adminService: AdminService) {}
   /*
   ngOnInit(): void {
     if (!this.voteService.isLoggedIn) {
