1b9684e695defccd86d53844a3731562823dc4b3
CaoAnton <antoncao1510@gmail.com>
2024-04-21 19:08:38 +0200

    feat: create chart for results

--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -1,109 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Election} from "../shared/entity/election-model";
 import {Result} from "../shared/entity/result";
-import {EmailModel} from "../shared/entity/email-model";
 import {ElectionService} from "../shared/control/election.service";
 import {Chart, registerables} from "chart.js";
 
 Chart.register(...registerables);
 
-declare var google: any;
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit{
   elections: Election[] | undefined = undefined;
   selectedElection: Election | undefined = this.electionService.selectedElection;
   result: Result[] | undefined = undefined;
-  emailInput: string = "";
-  emails: EmailModel[] | undefined;
-  emailError: boolean = false;
   resultError: boolean = false;
-  isCsvUploaded: boolean = false;
-  csvData: string = "";
-  myChart: any;
+  public chart: any;
 
-  constructor(
-    public electionService: ElectionService,
-  ) {
-    electionService.getList().forEach(value => {
-      this.elections = value;
-    });
-    this.getResult();
-    console.log("Result in Constructor ", this.result?.length);
-  }
-
+  constructor(public electionService: ElectionService) {}
 
   ngOnInit(): void {
-    this.RenderChart();
+    this.electionService.getList().forEach((value) => {
+      this.elections = value;
+    });
+    this.getResult()
   }
 
-  RenderChart() {
-    let candidates: string[] = [];
-    this.getResult();
-    if (this.result != undefined) {
-      console.log("Result in RenderChart ", this.result?.length);
-      for (let i = 0; i < this.result.length; i++) {
-        candidates.push(this.result[i].lastname);
-      }
-    }
-    new Chart("piechart", {
-      type: 'bar',
+  createChart(results: Result[]) {
+    const labels = results.map((r) => `${r.firstname} ${r.lastname} (${r.grade})`);
+    const data = results.map((r) => r.percentage);
+
+    this.chart = new Chart("Results", {
+      type: 'doughnut',
       data: {
-        labels: candidates,
-        datasets: [{
-          label: 'Number of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 2
-        }]
+        labels,
+        datasets: [{ label: 'Results', data }],
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
+      options: { aspectRatio: 2.5 },
     });
   }
-
-  onFileChange(event: any) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-      const fileType = file.name.split('.').pop()?.toLowerCase();
-      if (fileType !== 'csv') {
-        console.error('Not an CSV type');
-        event.target.value = '';
-        return;
-      }
-      this.isCsvUploaded = true;
-      this.csvData = reader.result?.toString() || '';
-    };
-    reader.readAsText(file);
-  }
-
-  parseCSV() {
-    const rows = this.csvData.split('\n');
-    // Entferne Leerzeichen und leere Zeilen
-    const data = rows.map(row => row.trim()).filter(row => row);
-    console.log(data)
-    if (this.selectedElection !== undefined &&
-      this.selectedElection.id !== null) {
-      this.electionService.addMultipleEmails(data, this.selectedElection.id.toString()).subscribe((value) => {
-          console.log(value);
-          this.loadEmails();
-          this.emailError = false;
-        },
-        (error) => {
-          this.emailError = true;
-        })
-    }
-  }
-
   getResult() {
     if (this.selectedElection !== undefined && this.selectedElection.id !== undefined && this.selectedElection.id !== null) {
       this.electionService.result(this.selectedElection.id.toString()).subscribe((value) => {
@@ -131,52 +68,9 @@ export class ResultsComponent {
         console.log("GetResults candidates: ", candidateResults.forEach(value => value.lastname));
         this.result = candidateResults;
         console.log("GetResults this.result: ", this.result.forEach(value => value));
-
-
+        this.createChart(candidateResults);
       }, (error) => {
         this.resultError = true;
-      });
-    }
-  }
-
-  addEmail() {
-    if (this.selectedElection !== undefined &&
-      this.selectedElection.id !== null) {
-      this.electionService.addEmail(this.emailInput, this.selectedElection.id.toString()).subscribe((value) => {
-          console.log(value);
-          this.loadEmails();
-          this.emailError = false;
-        },
-        (error) => {
-          this.emailError = true;
-        })
-    }
-  }
-
-  loadEmails() {
-    if (this.selectedElection !== undefined &&
-      this.selectedElection.id !== null) {
-      this.electionService.getMails(this.selectedElection.id.toString()).forEach(value => {
-        this.emails = value;
-      })
-    }
-  }
-
-  deleteEmail(id: number) {
-    if (Number(id)) {
-      this.electionService.removeMail(id.toString()).forEach(value => {
-        this.loadEmails();
-      });
-    }
-  }
-
-  sendCodes() {
-    if (this.selectedElection !== undefined &&
-      this.selectedElection.id !== null) {
-      this.electionService.sendCodes(this.selectedElection.id.toString()).subscribe((value) => {
-        console.log("emails sent");
-      }, (error) => {
-        console.log("error while sending mail");
       });
     }
   }
