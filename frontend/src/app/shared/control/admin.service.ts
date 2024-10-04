71c6027b09040ee34058b9d885fc34a1d5ec84cf
david <d.lehner2@students.htl-leonding.ac.at>
2024-10-04 19:23:31 +0200

    -fix: overworked isLoggedIn check. Should really check now. Solution before was not perfect

--- frontend/src/app/shared/control/admin.service.ts
+++ frontend/src/app/shared/control/admin.service.ts
@@ -6,7 +6,8 @@ import {JwtHelperService} from "@auth0/angular-jwt";
   providedIn: 'root'
 })
 export class AdminService {
-  isLoggedIn: boolean = this.keycloakService.getUserRoles().includes('default-roles-master');
+  //isLoggedIn: boolean = this.keycloakService.getUserRoles().includes('default-roles-master');
+  isLoggedIn: Promise<boolean> =this.keycloakService.isLoggedIn();
 
   constructor(private keycloakService: KeycloakService) {
     const helper = new JwtHelperService();
