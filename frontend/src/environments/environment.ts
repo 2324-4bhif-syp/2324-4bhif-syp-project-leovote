79c6f03db9de94d2cfafb5d20902c54a000a8f4b
david <d.lehner2@students.htl-leonding.ac.at>
2024-10-11 12:03:56 +0200

    -fix: changed environment to https for api (backend)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://leovote.htl-leonding.ac.at/api/',
+  apiUrl: 'https://leovote.htl-leonding.ac.at/api/',
   clientId: 'leovote'
 };
