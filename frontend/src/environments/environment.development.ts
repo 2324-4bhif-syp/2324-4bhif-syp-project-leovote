050e6c0d23de2d6813eb5fae49203ba141e40670
david <d.lehner2@students.htl-leonding.ac.at>
2024-12-13 22:06:49 +0100

    -feat: testing frontend deployment
    #244blv-152 Pipeline

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,11 +1,5 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:8080/',
-//   clientId: 'leovotetest'
-// };
-
 export const environment = {
-  production: true,
-  apiUrl: 'https://leovote.htl-leonding.ac.at/api/',
-  clientId: 'leovote'
+  production: false,
+  apiUrl: 'http://localhost:8080/',
+  clientId: 'leovotetest'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://leovote.htl-leonding.ac.at/api/',
+//   clientId: 'leovote'
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'https://leovote.htl-leonding.ac.at/api/',
-  clientId: 'leovote'
+  apiUrl: 'http://localhost:8080/',
+  clientId: 'leovotetest'
 };
