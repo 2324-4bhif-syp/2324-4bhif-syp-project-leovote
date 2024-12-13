b67c2692b2e0e239789b0fe39b22102622b95104
david <d.lehner2@students.htl-leonding.ac.at>
2024-12-13 21:41:26 +0100

    -feat: made frontend.yaml and reconfigured environment in angular for testing purposes
    #244blv-152 Pipeline

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/',
+//   clientId: 'leovotetest'
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/',
-  clientId: 'leovotetest'
+  production: true,
+  apiUrl: 'https://leovote.htl-leonding.ac.at/api/',
+  clientId: 'leovote'
 };
