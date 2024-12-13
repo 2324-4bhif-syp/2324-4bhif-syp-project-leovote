a735cd1e2c022e41aa29ee78394d29c0d6ea80e1
david <d.lehner2@students.htl-leonding.ac.at>
2024-12-13 16:29:26 +0100

    -fix: overworked file paths #244blv-152 Pipeline

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mvn -B package
-cp ./backend/src/main/docker/Dockerfile target/
+cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_REPOSITORY/deploy-pvc:latest ./target
 docker push ghcr.io/$GITHUB_REPOSITORY/deploy-pvc:latest
