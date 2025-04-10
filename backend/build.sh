a475b5a83ac8cc09fc157b99144701cdc9ee8bd3
david <d.lehner2@students.htl-leonding.ac.at>
2025-04-10 09:24:02 +0200

    -fix: updated build file

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mvn -B package
+mvn -B package -DskipTests
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
